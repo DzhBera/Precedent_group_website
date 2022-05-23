963b8ac601f0aa6df31476bdd8bdf1313d976827
Dzh.b <dzhanykulova.b@gmail.com>
2022-05-23 23:47:35 +0600

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,41 @@
-
 import clsx from "clsx";
 import css from "./footer.module.css";
-function Footer () {
-  
-    return (
-        <footer className={css.footer}>
-    <div className={css.container}>
-        <h1>Footer</h1>
-         </div>
-        </footer>
-        ) }
-        
-export default Footer; 
+import  LocationIcon  from "./../../assets/icons/location_icon.svg";
+import  EmailIcon  from "./../../assets/icons/email_icon.svg";
+import  PhoneIcon  from "./../../assets/icons/phone_icon.svg";
+function Footer() {
+  return (
+    <footer className={css.footer}>
+      <div className={css.container}>
+        <div className={css.phone}>
+        <img
+              src={PhoneIcon}
+             
+              alt="phone icon"
+            />
+          <span><a href="tel:996312906616">+996 312 906616</a></span>
+        </div>
+        <div className={css.email}>
+        <img
+              src={EmailIcon}
+              
+              alt="email icon"
+            />
+          
+          <span><a href="mailto:precedentoffice@gmail.com">precedentoffice@gmail.com</a></span>
+        </div>
+       
+        <address className={css.location}>
+        <img
+              src={LocationIcon}
+            
+              alt="location icon"
+            />
+          <a href="https://go.2gis.com/s91li"> Бишкек, ул. Сыдыкова 187 офис 28</a>
+        </address>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
