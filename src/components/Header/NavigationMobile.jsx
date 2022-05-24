fe963896b372b79a9e1d4fbc714066c6373370d0
Dzh.b <dzhanykulova.b@gmail.com>
2022-05-24 14:38:01 +0600

    added nav mobile icon

--- src/components/Header/NavigationMobile.jsx
+++ src/components/Header/NavigationMobile.jsx
@@ -1,10 +1,14 @@
 import css from './header.module.css';
-
+import  MobileIcon from "./../../assets/icons/menu_icon.svg";
 function NavigationMobile () {
     return (
-        <h1 className="hidden">
-          Hello
-      </h1>
+      <nav className={css.mobile}>
+       <img
+              src={MobileIcon}
+             
+              alt="mobile navigation icon"
+            />
+      </nav>
     )
 }
 export default NavigationMobile; 
